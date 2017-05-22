f285a9fa5a7897743aea9caff56760f476a09dde
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-05-22 12:02:53 +0200

    fix travis i hope so

--- ci.sh
+++ ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[ImagicalMine] Adding "
+echo -e "version\nmakeserver\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[ImagicalMine] Adding "
 if ls plugins/Tesseract/ImagicalMine*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
