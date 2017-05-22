006a29c16602eda3179a813a3e1c62eb413048e3
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-05-22 12:12:03 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo -e "version\nmakeserver\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[ImagicalMine] Adding "
-if ls plugins/Tesseract/ImagicalMine*.phar >/dev/null 2>&1; then
+echo -e "version\nmakeserver\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard
+if ls plugins/Tesseract/ImagicalMine.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
     echo No phar created!
